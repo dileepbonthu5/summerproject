2741ae6c80de486918bbdc5ed3f4c44238967c93
dileepbonthu5 <bonthud1@newpaltz.edu>
2022-07-08 23:45:08 +0300

    Commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 export async function fetchData(route='', data={}, methodType) {
   //sending over our data to specified route in server
-  const response = await fetch(`http://localhost:3001${route}`, {
+  const response = await fetch(`https://bonthusumbeproject.herokuapp.com/${route}`, {
     method: methodType,
     headers: {
       'Content-Type': 'application/json'
